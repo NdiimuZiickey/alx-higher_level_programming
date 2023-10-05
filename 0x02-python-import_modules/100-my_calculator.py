980b6c033bc8f39baba74356aaf9523b10df196d
NdiimuZiickey <zeeisa518@gmail.com>
2023-10-05 12:38:30 -0700

    update

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,20 +1,28 @@
 #!/usr/bin/python3
+# 100-my_calculator.py
+
+"""Import all functions from calculator_1.py and handle basic operations"""
+
 
 if __name__ == "__main__":
-    """Handle basic arithmetic operations."""
-    from calculator_1 import add, sub, mul, div 
     import sys
-
-    if len(sys.argv) - 1 != 3:
+    from calculator_1 import add, div, sub, mul
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
+    n1, op, n2 = int(sys.argv[1]), sys.argv[2], int(sys.argv[3])
 
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
+    def error(n, nn):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
-
+    def switch(op):
+        switcher = {
+                "+": add,
+                "-": sub,
+                "*": mul,
+                "/": div,
+                }
+        return switcher.get(op, error)
+    r = switch(op)(n1, n2)
+    print(f"{n1} {op} {n2} = {r}")
