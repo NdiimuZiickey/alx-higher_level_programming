0e27be0216c95539c42bce3d0135f2c9f8f91842
NdiimuZiickey <zeeisa518@gmail.com>
2024-02-20 13:53:37 -0800

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,15 +1,11 @@
 #!/usr/bin/node
 
-const fs = require('fs');
-
-const file = process.argv[2];
-
-
-
-fs.readFile(file, 'utf-8', (err, data) => {
-	if (err) {
-		console.error(err);
-	} else {
-		console.log(data);
-	}
-});
+const filesys = require('fs');
+filesys.readFile(process.argv[2], 'utf-8',
+	  function (err, data) {
+		      if (err) {
+			            console.log(err);
+			            return;
+			          }
+		      console.log(data);
+		    });
